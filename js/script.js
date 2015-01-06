1e5a6c18da11e1ee848ad4893eb4688c09fb749e
Brian Su <briansudev@gmail.com>
2015-01-06 02:08:37 -0800

    almost done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
     anchors: ['intro', 'about', 'projects', 'connect'],
-    sectionsColor: ['#C63D0F', '#36465d', '#a77dc2', '#7E8F7C'],
+    sectionsColor: ['#C63D0F', '#36465d', '#a77dc2', '#f2992e'],
     navigation: true,
     navigationPosition: 'left',
     navigationTooltips: ['Intro', 'About', 'Projects', 'Connect']
